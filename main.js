ebab5cb903c800acc5312111aed17ee9b0129fc8
Hagar Shilo <strayblues@gmail.com>
2017-07-28 18:04:32 +0300

    Clean up variables

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 "use strict";
 
-var hex;
-var rgb;
-var hexArray;
-var rgbArray = [];
-var r;
-var g;
-var b;
-var regex = /^[a-z]+$/i;
+const REGEX = /^[a-z]+$/i;
+var characterArray;
+var digitArray;
 
 function hexToRgb() {
-  hex = document.getElementById('hex-input').value;
+  var hex = document.getElementById('hex-input').value;
   if (hex != null) { // TODO Other input validation conditions may be wanted
-    hexArray = hex.split('');
+    // Array contains 6 single character strings of digits and letters
+    characterArray = hex.split('');
     handleLetters();
     // Output the result
     document.getElementById('rgb-output').innerHTML = toRGB();
@@ -23,11 +19,11 @@ function hexToRgb() {
 }
 
 function handleLetters(){
-  for (var i=0; i<hexArray.length; i++) {
+  for (var i=0; i<characterArray.length; i++) {
     // If the array contains a letter
-    if(hexArray[i].match(regex)) {
+    if(characterArray[i].match(REGEX)) {
       // Convert letter to number
-      hexArray[i] = letterToNumber(hexArray[i]);
+      characterArray[i] = letterToNumber(characterArray[i]);
     }
   }
 }
@@ -55,13 +51,15 @@ function letterToNumber(l){
 }
 
 function toRGB(){
-  for(var i=0; i<hexArray.length; i++){
+  var rgbArray = [];
+  var digitArray = characterArray;
+  for(var i=0; i<digitArray.length; i++){
     if (i % 2 === 0) {
-      var leftNum = parseInt(hexArray[i]);
-      leftNum = leftNum*16;
-      var rightNum = parseInt(hexArray[i+1]);
-      leftNum = leftNum + rightNum;
-      rgbArray.push(leftNum);
+      var leftDig = parseInt(digitArray[i]);
+      leftDig = leftDig*16;
+      var rightDig = parseInt(digitArray[i+1]);
+      leftDig = leftDig + rightDig;
+      rgbArray.push(leftDig);
     }
   }
   return 'rgb(' + rgbArray[0] + ', ' + rgbArray[1] + ', ' + rgbArray[2] + ')';
