6351c07a14f1b3de2c0963bf66d327ec7bd60d57
Hagar Shilo <strayblues@gmail.com>
2017-07-28 18:57:12 +0300

    Make toRGB Great Again

--- main.js
+++ main.js
@@ -57,12 +57,10 @@ function letterToNumber(l){
 
 function toRGB(digitArray){
   var rgbArray = [];
-  for(var i=0; i<digitArray.length; i++){
-    if (i % 2 === 0) {
+  for(var i=0; i<digitArray.length; i = i+2){
       var leftDig = digitArray[i];
       var rightDig = digitArray[i+1];
       rgbArray.push(leftDig*16 + rightDig);
-    }
   }
   return 'rgb(' + rgbArray[0] + ', ' + rgbArray[1] + ', ' + rgbArray[2] + ')';
 }
